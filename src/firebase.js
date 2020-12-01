285bd1fc3cb405339bf6e4fc704a613af3da3158
Scorpioeric-dev <ericscorpio1@gmail.com>
2020-12-01 07:49:32 -0800

    amazon

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDB8mo9-JyrxC2pJBkN77jf5MEEiQhGHwg",
+    authDomain: "clone-eeef6.firebaseapp.com",
+    databaseURL: "https://clone-eeef6.firebaseio.com",
+    projectId: "clone-eeef6",
+    storageBucket: "clone-eeef6.appspot.com",
+    messagingSenderId: "845213143786",
+    appId: "1:845213143786:web:3cd29f9989a14bb98efd93"
+  };

4f4f0f565d26e51897bb6fda4e378b365d80a852
Scorpioeric-dev <ericscorpio1@gmail.com>
2020-12-15 20:04:31 -0800

    payment-component

--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -1,21 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../Style/Payment.css";
 import { useStateValue } from "../UseContext/StateProvider";
 import CheckoutProduct from "../Components/CheckoutProduct";
-import { Link } from "react-router-dom";
+import { Link,useHistory } from "react-router-dom";
 //grabbing the HOC's from Stripe
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "../Reducer";
+import axios from "axios";
+
+
 
 const Payment = () => {
   const [{ basket, user }, dispatch] = useStateValue();
+  const history = useHistory()
+
   const stripe = useStripe();
   const elements = useElements();
+
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
   const [error, setError] = useState(null);
   const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState(true);
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    //generate the special stripe secret which allows us to charge the customer
+    const getClientSecret = async () => {
+      const response = await axios.post(
+        //Stripe expects the total in a currencies subunit 1 dollar = 100 cents
+        `/payments/create?total=${getBasketTotal(basket) * 100}`
+      );
+      setClientSecret(response.data.clientSecret);
+    };
+
+    getClientSecret();
+  }, [basket]);
+
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setProcessing(true); //Controls the button
+
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      })
+      .then(({ paymentIntent }) => {
+        //payment Intent = payment confirmation
+        setSucceeded(true);
+        setError(null);
+        setProcessing(false);
+      
+        history.replace("/orders");
+      });
+
     //Stripe stuff
   };
 
@@ -89,7 +130,11 @@ const Payment = () => {
                   thousandSeparator={true}
                   prefix={"$"}
                 />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
               </div>
+              {error && <div>{error}</div>}
             </form>
           </div>
         </div>
